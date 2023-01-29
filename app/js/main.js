19eebb767b49e2de89a2765003cc08e81c0c8731
StanislavSlobodenyuk <116057509+StanislavSlobodenyuk@users.noreply.github.com>
2023-01-29 16:51:03 +0200

    End header + added footer (with adaptive)

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,19 @@
 const iconMenu = document.querySelector(".menu__icon");
-const headerMenu = document.querySelector('.header__menu');
+const headerMenu = document.querySelector('.header-menu_mobile');
+const headerClose = document.querySelector('.header-menu__close');
+
 if (iconMenu) {
-    iconMenu.addEventListener("click", function(e){
+    iconMenu.addEventListener("click", function(elem){
         document.body.classList.toggle('._lock');
         iconMenu.classList.toggle('_active');
         headerMenu.classList.toggle('_active');
     });
+
+}
+if (headerClose) {
+    headerClose.addEventListener("click", function (elem) {
+        document.body.classList.toggle('._lock');
+        iconMenu.classList.toggle('_active');
+        headerMenu.classList.toggle('_active');
+    })
 }
